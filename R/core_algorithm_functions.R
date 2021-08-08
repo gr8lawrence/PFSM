60553ab695f2e0816e54b1b5f96a97cb8ffcd8bf
Tianyi Liu <gr8lawrence@Tianyis-MacBook-Pro.local>
2021-08-08 12:21:51 -0400

    Restructured the algorithm functions

---
+++ R/core_algorithm_functions.R
@@ -0,0 +1,11 @@
+## the follwing functions are for iterative solving the optimization problem
+## paramaeters:
+## G0: partially known signature matrix (constructed separately)
+## G_init: initial value for G
+
+
+PSMF_solve <- function(G0, G_init) {
+  
+  
+  
+}
